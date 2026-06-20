61dbdee108ef469a231cc3235c075e5963e04a1a
formmantg <formicki.elation@gmail.com>
2026-06-20 08:26:13 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import { WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({
+  port: process.env.PORT || 8080
+});
 
-console.log("WS server running on ws://localhost:8080");
+console.log("WS running");
 
 function broadcast(data) {
   const msg = JSON.stringify(data);
 
-  wss.clients.forEach((client) => {
+  wss.clients.forEach(client => {
     if (client.readyState === 1) {
       client.send(msg);
     }
@@ -17,22 +19,22 @@ function broadcast(data) {
 wss.on("connection", (ws) => {
   console.log("user joined");
 
-  // event dla wszystkich
   broadcast({ type: "user_joined" });
+
+  ws.on("message", (msg) => {
+    try {
+      const data = JSON.parse(msg);
+
+      if (data.type === "pulse") {
+        broadcast({ type: "pulse" });
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  });
 
   ws.on("close", () => {
     console.log("user left");
-
     broadcast({ type: "user_left" });
-  });
-
-  ws.on("message", (data) => {
-    try {
-      const msg = JSON.parse(data);
-
-      if (msg.type === "pulse") {
-        broadcast({ type: "pulse" });
-      }
-    } catch (e) {}
   });
 });
